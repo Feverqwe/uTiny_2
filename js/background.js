dae096e698cbaeedbb01ad44e560b6f1e2f3cb9a
Anton <anton9121@gmail.com>
2013-10-19 21:24:34 +0600

    2.1.2

--- js/background.js
+++ js/background.js
@@ -460,7 +460,6 @@ var engine = function() {
         var downloadFile = function(url, callback) {
             var xhr = new XMLHttpRequest();
             xhr.open("GET", url, true);
-            xhr.overrideMimeType("text/plain; charset=x-user-defined");
             xhr.responseType = "blob";
             xhr.onload = function() {
                 callback(xhr.response);
