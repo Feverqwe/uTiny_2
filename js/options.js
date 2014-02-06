2fe0e8d7d668ca137157c1032fed1a97e7b01aa6
Anton <anton9121@gmail.com>
2014-02-06 10:04:47 +0600

    fix

--- js/options.js
+++ js/options.js
@@ -314,12 +314,12 @@ var options = function() {
                 $('select[name="folders"] :selected').remove();
             });
             $('input[name="option_down"]').on('click', function() {
-                var $this = $('select[name="folders"] :selected');
+                var $this = $('select[name="folders"] :selected').eq(0);
                 var $next = $this.next();
                 $next.after($this);
             });
             $('input[name="option_up"]').on('click', function() {
-                var $this = $('select[name="folders"] :selected');
+                var $this = $('select[name="folders"] :selected').eq(0);
                 var $next = $this.prev();
                 $next.before($this);
             });
