dc1d6ada892e92ec63940b4533fa3bfd5cb49744
Anton <anton9121@gmail.com>
2014-07-10 10:10:23 +0600

    fix

--- js/background.js
+++ js/background.js
@@ -1035,7 +1035,10 @@ var engine = function () {
             }
             if ( ['number', 'checkbox'].indexOf(def_item.t) !== -1 ) {
                 value = parseInt(value);
-            }
+                if (!isNaN(value)) {
+                    settings[key] = value;
+                }
+            } else
             if (value) {
                 settings[key] = value;
             }
