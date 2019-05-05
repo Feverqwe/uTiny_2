06f4fa9756dca051121841f4c27ff620c3203856
Anton <leonardspbox@gmail.com>
2019-05-05 10:34:57 +0500

    1

--- src/bg/contextMenu.js
+++ src/bg/contextMenu.js
@@ -133,7 +133,7 @@ class ContextMenu {
   createFolderMenu(parentId) {
     const folders = this.bgStore.config.folders;
     if (this.bgStore.config.treeViewContextMenu) {
-      this.transformFoldersToTree(folders).forEach((folder) => {
+      transformFoldersToTree(folders).forEach((folder) => {
         let name = folder.name;
         if (name === './') {
           name = chrome.i18n.getMessage('currentDirectory');
@@ -196,108 +196,108 @@ class ContextMenu {
     }
   }
 
-  transformFoldersToTree(folders) {
-    const placeFolderMap = {};
-    const places = [];
-
-    let sep = null;
-    
-    folders.forEach((folder) => {
-      const place = folder.path;
-      if (sep === null) {
-        sep = /\//.test(place) ? '/' : '\\';
-      }
-      let normPath = place.split(/[\\/]/).join('/');
-      normPath = path.normalize(normPath);
-      if (/\/$/.test(normPath)) {
-        normPath = normPath.slice(0, -1);
-      }
-      placeFolderMap[normPath] = folder;
-      places.push(normPath);
-    });
-
-    const lowKeyMap = {};
-    const tree = {};
-    places.forEach((place) => {
-      const parts = place.split('/');
-      if (parts[0] === '') {
-        parts.unshift(parts.splice(0, 2).join(sep));
-      }
-
-      let parentThree = tree;
-      parts.forEach((part, index) => {
-        const lowPart = parts.slice(0, index + 1).join('/').toLowerCase();
-        let caseKey = lowKeyMap[lowPart];
-        if (!caseKey) {
-          caseKey = lowKeyMap[lowPart] = part;
-        }
-        let subTree = parentThree[caseKey];
-        if (!subTree) {
-          subTree = parentThree[caseKey] = {};
-        }
-        if (index === parts.length - 1) {
-          subTree['./'] = place;
-        }
-        parentThree = subTree;
-      });
-    });
-
-    const joinSingleParts = (tree, part) => {
-      if (typeof tree !== 'object') return;
-      const subTree = tree[part];
-      const subParts = Object.keys(subTree);
-      if (subParts.length === 1) {
-        const subPart = subParts.shift();
-        if (subPart === './') {
-          tree[part] = subTree[subPart];
-        } else {
-          const joinedPart = part + sep + subPart;
-          delete tree[part];
-          tree[joinedPart] = subTree[subPart];
-          joinSingleParts(tree, joinedPart);
-        }
-      } else {
-        subParts.forEach((part) => {
-          joinSingleParts(subTree, part);
-        });
-      }
-    };
-    Object.keys(tree).forEach((part) => {
-      joinSingleParts(tree, part);
-    });
-
-    const menus = [];
-    const makeMenuItems = (tree, parentId) => {
-      Object.entries(tree).forEach(([name, item]) => {
-        if (typeof item === 'object') {
-          const branch = item;
-          const id = JSON.stringify({type: 'branch', index: menus.length});
-          menus.push({
-            name: name,
-            id,
-            parentId
-          });
-          makeMenuItems(branch, id);
-        } else {
-          const place = item;
-          const folder = placeFolderMap[place];
-          const id = JSON.stringify({type: 'folder', index: folders.indexOf(folder)});
-          menus.push(Object.assign(folder, {
-            name,
-            id,
-            parentId
-          }));
-        }
-      });
-    };
-    makeMenuItems(tree);
-
-    return menus;
-  }
-
   destroy() {
 
   }
+}
+
+function transformFoldersToTree(folders) {
+  const placeFolderMap = {};
+  const places = [];
+
+  let sep = null;
+
+  folders.forEach((folder) => {
+    const place = folder.path;
+    if (sep === null) {
+      sep = /\//.test(place) ? '/' : '\\';
+    }
+    let normPath = place.split(/[\\/]/).join('/');
+    normPath = path.normalize(normPath);
+    if (/\/$/.test(normPath)) {
+      normPath = normPath.slice(0, -1);
+    }
+    placeFolderMap[normPath] = folder;
+    places.push(normPath);
+  });
+
+  const lowKeyMap = {};
+  const tree = {};
+  places.forEach((place) => {
+    const parts = place.split('/');
+    if (parts[0] === '') {
+      parts.unshift(parts.splice(0, 2).join(sep));
+    }
+
+    let parentThree = tree;
+    parts.forEach((part, index) => {
+      const lowPart = parts.slice(0, index + 1).join('/').toLowerCase();
+      let caseKey = lowKeyMap[lowPart];
+      if (!caseKey) {
+        caseKey = lowKeyMap[lowPart] = part;
+      }
+      let subTree = parentThree[caseKey];
+      if (!subTree) {
+        subTree = parentThree[caseKey] = {};
+      }
+      if (index === parts.length - 1) {
+        subTree['./'] = place;
+      }
+      parentThree = subTree;
+    });
+  });
+
+  const joinSingleParts = (tree, part) => {
+    if (typeof tree !== 'object') return;
+    const subTree = tree[part];
+    const subParts = Object.keys(subTree);
+    if (subParts.length === 1) {
+      const subPart = subParts.shift();
+      if (subPart === './') {
+        tree[part] = subTree[subPart];
+      } else {
+        const joinedPart = part + sep + subPart;
+        delete tree[part];
+        tree[joinedPart] = subTree[subPart];
+        joinSingleParts(tree, joinedPart);
+      }
+    } else {
+      subParts.forEach((part) => {
+        joinSingleParts(subTree, part);
+      });
+    }
+  };
+  Object.keys(tree).forEach((part) => {
+    joinSingleParts(tree, part);
+  });
+
+  const menus = [];
+  const makeMenuItems = (tree, parentId) => {
+    Object.entries(tree).forEach(([name, item]) => {
+      if (typeof item === 'object') {
+        const branch = item;
+        const id = JSON.stringify({type: 'branch', index: menus.length});
+        menus.push({
+          name: name,
+          id,
+          parentId
+        });
+        makeMenuItems(branch, id);
+      } else {
+        const place = item;
+        const folder = placeFolderMap[place];
+        const id = JSON.stringify({type: 'folder', index: folders.indexOf(folder)});
+        menus.push(Object.assign(folder, {
+          name,
+          id,
+          parentId
+        }));
+      }
+    });
+  };
+  makeMenuItems(tree);
+
+  return menus;
 }
 
 export default ContextMenu;
