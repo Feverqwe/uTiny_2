752cb9693f527a9963d36fdfa14f95fcb4db891d
Anton <leonardspbox@gmail.com>
2019-05-11 07:56:53 +0500

    compressSource

--- builder/compressDist.js
+++ builder/compressDist.js
@@ -1,41 +1,13 @@
 require('./defaultBuildEnv');
 const path = require('path');
-const fs = require('fs');
-const archiver = require('archiver');
+const zipDirectory = require('./zipDirectory');
 
 const compressDist = () => {
   const ext = 'zip';
   const dist = BUILD_ENV.outputPath;
   const outputPath = path.join(BUILD_ENV.outputPath, '../');
 
-  const zipFolder = (srcFolder, zipFilePath, callback) => {
-    const output = fs.createWriteStream(zipFilePath);
-    const zipArchive = archiver('zip', {
-      zlib: { level: 9 }
-    });
-
-    output.on('close', function() {
-      callback();
-    });
-
-    zipArchive.pipe(output);
-
-    zipArchive.glob('**/*', {
-      cwd: srcFolder
-    });
-
-    zipArchive.finalize(function(err, bytes) {
-      if(err) {
-        callback(err);
-      }
-    });
-  };
-
-  return new Promise((resolve, reject) => {
-    zipFolder(dist, path.join(outputPath, `${BUILD_ENV.distName}.${ext}`), err => {
-      err ? reject(err) : resolve();
-    })
-  });
+  return zipDirectory({dirs: [dist]}, path.join(outputPath, `${BUILD_ENV.distName}.${ext}`));
 };
 
 compressDist();
