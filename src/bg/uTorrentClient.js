0128715bb44fcc6b848a5426ff23ffddc217642d
Anton <leonardspbox@gmail.com>
2019-05-26 07:37:51 +0500

    downloadFileFromUrl

--- src/bg/uTorrentClient.js
+++ src/bg/uTorrentClient.js
@@ -5,6 +5,7 @@ import getLogger from "../tools/getLogger";
 import queryStringify from "../tools/utQueryStringify";
 import splitByPart from "../tools/splitByPart";
 import arrayDifferent from "../tools/arrayDifferent";
+import downloadFileFromUrl from "../tools/downloadFileFromUrl";
 
 const logger = getLogger('UTorrentClient');
 
@@ -240,38 +241,19 @@ class UTorrentClient {
 
   sendFiles(urls, directory, label) {
     return Promise.all(urls.map((url) => {
-      return Promise.resolve().then(() => {
-        if (!/^(blob|https?):/.test(url)) {
-          return {url};
+      return downloadFileFromUrl(url).catch((err) => {
+        if (err.code === 'FILE_SIZE_EXCEEDED') {
+          this.bg.torrentErrorNotify(chrome.i18n.getMessage('fileSizeError'));
+          throw err;
         }
-
-        return fetch(url).then(response => {
-          if (!response.ok) {
-            throw new ErrorWithCode(`${response.status}: ${response.statusText}`, `RESPONSE_IS_NOT_OK`);
-          }
-
-          if (response.headers.get('Content-Length') > 1024 * 1024 * 10) {
-            throw new ErrorWithCode(`Size is more then 10mb`, 'FILE_SIZE_EXCEEDED');
-          }
-
-          return response.blob();
-        }).then((blob) => {
-          if (/^blob:/.test(url)) {
-            URL.revokeObjectURL(url);
-          }
-          return {blob};
-        }, (err) => {
-          if (err.code === 'FILE_SIZE_EXCEEDED') {
-            this.bg.torrentErrorNotify(chrome.i18n.getMessage('fileSizeError'));
-            throw err;
-          }
-          if (!/^https?:/.test(url)) {
-            this.bg.torrentErrorNotify(chrome.i18n.getMessage('unexpectedError'));
-            throw err;
-          }
-          logger.error('download url error, fallback to url', err);
-          return {url};
-        });
+        if (!/^https?:/.test(url)) {
+          this.bg.torrentErrorNotify(chrome.i18n.getMessage('unexpectedError'));
+          throw err;
+        }
+        if (err.code !== 'LINK_IS_NOT_SUPPORTED') {
+          logger.error('sendFiles: downloadFileFromUrl error, fallback to url', err);
+        }
+        return {url};
       }).then((data) => {
         return this.putTorrent(data, directory, label);
       }).then(() => {
